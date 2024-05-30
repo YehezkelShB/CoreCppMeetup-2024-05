7fb6356f70de0daa80ced5e7670beabeedc99f0a
Yehezkel Bernat <YehezkelShB@gmail.com>
2024-05-30 16:54:09 +0300

    Support custom format specifier

---
+++ include/Complex.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <format>
+#include <cmath>
+#include <numbers>
+
+class Complex
+{
+public:
+	Complex(double real, double imag)
+		: m_real(real), m_imag(imag)
+	{
+	}
+
+	double real() const
+	{
+		return m_real;
+	}
+
+	double imag() const
+	{
+		return m_imag;
+	}
+
+	double radius() const
+	{
+		return std::hypot(m_real, m_imag);
+	}
+
+	double angle() const
+	{
+		return std::atan2(m_imag, m_real) * (180.0 / std::numbers::pi_v<double>);
+	}
+
+private:
+	double m_real;
+	double m_imag;
+};
+
+template <>
+struct std::formatter<Complex>
+{
+	bool m_polar = false;
+
+	constexpr auto parse(auto& ctx)
+	{
+		auto it = ctx.begin();
+
+		if (it == ctx.end() or *it == '}')
+		{
+			return it;
+		}
+
+		switch (*it++)
+		{
+		case 'p':
+			m_polar = true;
+			break;
+		default:
+			throw std::format_error("invalid format");
+		}
+
+		return it;
+	}
+
+	auto format(const Complex& complex, auto& ctx) const
+	{
+		if (m_polar)
+		{
+			return std::format_to(ctx.out(), "({} * e^({}i))", complex.radius(), complex.angle());
+		}
+		return std::format_to(ctx.out(), "({} + {}i)", complex.real(), complex.imag());
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "MyType.h"
 #include "MyString.h"
+#include "Complex.h"
 
 #include <iostream>
 
@@ -18,5 +19,11 @@ int main()
 		MyString str("Hello, world!");
 		std::print("{}\n", str);
 		//std::print("{:*^20}\n", str);
+	}
+
+	{
+		Complex complex(1.0, 2.0);
+		std::print("{}\n", complex);
+		std::print("{:p}\n", complex);
 	}
 }
